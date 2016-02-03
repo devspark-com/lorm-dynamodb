317c691cbcec1b4b779fd391fa3460fecc716e1b
Javier Storni <storni@gmail.com>
2016-02-03 14:36:45 -0300

    Added more robust delete table operation in integration test

--- src/test/java/org/devspark/aws/lorm/dynamodb/it/schema/DynamoDBBaseRepositoryTest.java
+++ src/test/java/org/devspark/aws/lorm/dynamodb/it/schema/DynamoDBBaseRepositoryTest.java
@@ -15,173 +15,182 @@ import org.junit.Test;
 
 public class DynamoDBBaseRepositoryTest extends BaseIntegrationTest {
 
-    @Before
-    public void setup() {
-	super.setup();
+	@Before
+	public void setup() {
+		super.setup();
 
-	addToEntityManager(Merchant.class);
-	addToEntityManager(Expense.class);
-	addToEntityManager(SampleEntity.class);
+		addToEntityManager(Merchant.class);
+		addToEntityManager(Expense.class);
+		addToEntityManager(SampleEntity.class);
 
-	setupSchemaForEntity(Merchant.class);
-	setupSchemaForEntity(Expense.class);
-	setupSchemaForEntity(SampleEntity.class);
+		setupSchemaForEntity(Merchant.class);
+		setupSchemaForEntity(Expense.class);
+		setupSchemaForEntity(SampleEntity.class);
 
-	// remove all merchants
+		// remove all merchants
 
-	Repository<Merchant> merchantRepository = entityManager.getRepository(Merchant.class);
+		Repository<Merchant> merchantRepository = entityManager
+				.getRepository(Merchant.class);
 
-	List<String> existingMerchantIds = new ArrayList<String>();
-	List<Merchant> existingMerchants = merchantRepository.findAll();
-	for (Merchant merchant : existingMerchants) {
-	    existingMerchantIds.add(merchant.getId());
-	}
-	merchantRepository.deleteById(existingMerchantIds);
-
-	// remove all expenses
-	Repository<Expense> expenseRepository = entityManager.getRepository(Expense.class);
-
-	List<String> existingExpenseIds = new ArrayList<String>();
-	List<Expense> existingExpenses = expenseRepository.findAll();
-	for (Expense expense : existingExpenses) {
-	    existingExpenseIds.add(expense.getId());
-	}
-	expenseRepository.deleteById(existingExpenseIds);
-
-    }
-
-    private Merchant buildMerchant(String name) {
-	Merchant merchant = new Merchant();
-	merchant.setName(name);
-
-	return merchant;
-    }
-
-    @Test
-    public void testEmbedded() {
-	SampleEntity sampleEntity = new SampleEntity();
-	sampleEntity.setId("abarajame");
-	sampleEntity.setSomeRandomField("random field");
-	sampleEntity.setEmbedded(buildSampleEmbeddable());
-
-	Repository<SampleEntity> repository = entityManager.getRepository(SampleEntity.class);
-
-	repository.save(sampleEntity);
-
-	SampleEntity savedSampleEntity = repository.findOne("abarajame");
-
-	Assert.assertNotNull(savedSampleEntity);
-	Assert.assertNotNull(savedSampleEntity.getEmbedded());
-	Assert.assertNotNull(savedSampleEntity.getEmbedded().getDeepEmbedded());
-
-	Assert.assertEquals("random field", savedSampleEntity.getSomeRandomField());
-	Assert.assertEquals("some embedded field", savedSampleEntity.getEmbedded().getSomeField());
-	Assert.assertEquals("some deep embedded field",
-		savedSampleEntity.getEmbedded().getDeepEmbedded().getEmbeddedField());
-    }
-
-    private SampleEmbeddable buildSampleEmbeddable() {
-	DeepEmbedded deepEmbedded = new DeepEmbedded();
-	deepEmbedded.setEmbeddedField("some deep embedded field");
-
-	SampleEmbeddable sampleEmbeddable = new SampleEmbeddable();
-	sampleEmbeddable.setSomeField("some embedded field");
-	sampleEmbeddable.setDeepEmbedded(deepEmbedded);
-
-	return sampleEmbeddable;
-    }
-
-    @Test
-    public void testSingle() {
-	Repository<Merchant> repository = entityManager.getRepository(Merchant.class);
-
-	// create one
-	Merchant merchant = buildMerchant("new merchant");
-
-	Merchant savedMerchant = repository.save(merchant);
-
-	Assert.assertTrue(merchant == savedMerchant); // same reference
-	Assert.assertNotNull(merchant.getId());
-	Assert.assertEquals("new merchant", merchant.getName());
-
-	// find by id
-	Merchant foundMerchant = repository.findOne(merchant.getId());
-	Assert.assertNotNull(foundMerchant);
-	Assert.assertEquals(merchant.getId(), foundMerchant.getId());
-	Assert.assertEquals("new merchant", foundMerchant.getName());
-
-	// update
-	foundMerchant.setName("updated name");
-	repository.save(foundMerchant);
-	Merchant updatedMerchant = repository.findOne(merchant.getId());
-	Assert.assertNotNull(updatedMerchant);
-	Assert.assertEquals(merchant.getId(), updatedMerchant.getId());
-	Assert.assertEquals("updated name", updatedMerchant.getName());
-
-	// delete
-	repository.deleteById(merchant.getId());
-	Merchant deletedMerchant = repository.findOne(merchant.getId());
-	Assert.assertNull(deletedMerchant);
-    }
-
-    @Test
-    public void testBatch() {
-	Repository<Merchant> repository = entityManager.getRepository(Merchant.class);
-
-	// add 100
-	List<Merchant> merchants = new ArrayList<Merchant>();
-	for (int i = 0; i < 100; i++) {
-	    Merchant merchant = buildMerchant("sample merchant #" + i);
-	    merchants.add(merchant);
-	}
-
-	repository.save(merchants);
-
-	// fetch recently added
-	List<Merchant> savedMerchants = repository.findAll();
-
-	Assert.assertNotNull(savedMerchants);
-	Assert.assertEquals(merchants.size(), savedMerchants.size());
-
-	for (Merchant merchant : merchants) {
-	    boolean found = false;
-	    for (Merchant savedMerchant : savedMerchants) {
-		if (merchant.getId().equals(savedMerchant.getId())) {
-		    Assert.assertEquals(merchant.getName(), savedMerchant.getName());
-		    found = true;
-		    break;
+		List<String> existingMerchantIds = new ArrayList<String>();
+		List<Merchant> existingMerchants = merchantRepository.findAll();
+		for (Merchant merchant : existingMerchants) {
+			existingMerchantIds.add(merchant.getId());
 		}
-	    }
+		merchantRepository.deleteById(existingMerchantIds);
 
-	    Assert.assertTrue("Merchant not found", found);
-	}
+		// remove all expenses
+		Repository<Expense> expenseRepository = entityManager
+				.getRepository(Expense.class);
 
-	// update all
-	for (Merchant merchant : merchants) {
-	    merchant.setName("updated name");
-	}
-
-	repository.save(merchants);
-
-	// check batch update
-	List<Merchant> updatedMerchants = repository.findAll();
-
-	Assert.assertNotNull(updatedMerchants);
-	Assert.assertEquals(merchants.size(), updatedMerchants.size());
-
-	for (Merchant merchant : merchants) {
-	    boolean found = false;
-	    for (Merchant updatedMerchant : updatedMerchants) {
-		if (merchant.getId().equals(updatedMerchant.getId())) {
-		    Assert.assertEquals("updated name", updatedMerchant.getName());
-		    found = true;
-		    break;
+		List<String> existingExpenseIds = new ArrayList<String>();
+		List<Expense> existingExpenses = expenseRepository.findAll();
+		for (Expense expense : existingExpenses) {
+			existingExpenseIds.add(expense.getId());
 		}
-	    }
+		expenseRepository.deleteById(existingExpenseIds);
 
-	    Assert.assertTrue("Merchant not found", found);
 	}
 
-    }
+	private Merchant buildMerchant(String name) {
+		Merchant merchant = new Merchant();
+		merchant.setName(name);
+
+		return merchant;
+	}
+
+	@Test
+	public void testEmbedded() {
+		SampleEntity sampleEntity = new SampleEntity();
+		sampleEntity.setId("abarajame");
+		sampleEntity.setSomeRandomField("random field");
+		sampleEntity.setEmbedded(buildSampleEmbeddable());
+
+		Repository<SampleEntity> repository = entityManager
+				.getRepository(SampleEntity.class);
+
+		repository.save(sampleEntity);
+
+		SampleEntity savedSampleEntity = repository.findOne("abarajame");
+
+		Assert.assertNotNull(savedSampleEntity);
+		Assert.assertNotNull(savedSampleEntity.getEmbedded());
+		Assert.assertNotNull(savedSampleEntity.getEmbedded().getDeepEmbedded());
+
+		Assert.assertEquals("random field",
+				savedSampleEntity.getSomeRandomField());
+		Assert.assertEquals("some embedded field", savedSampleEntity
+				.getEmbedded().getSomeField());
+		Assert.assertEquals("some deep embedded field", savedSampleEntity
+				.getEmbedded().getDeepEmbedded().getEmbeddedField());
+	}
+
+	private SampleEmbeddable buildSampleEmbeddable() {
+		DeepEmbedded deepEmbedded = new DeepEmbedded();
+		deepEmbedded.setEmbeddedField("some deep embedded field");
+
+		SampleEmbeddable sampleEmbeddable = new SampleEmbeddable();
+		sampleEmbeddable.setSomeField("some embedded field");
+		sampleEmbeddable.setDeepEmbedded(deepEmbedded);
+
+		return sampleEmbeddable;
+	}
+
+	@Test
+	public void testSingle() {
+		Repository<Merchant> repository = entityManager
+				.getRepository(Merchant.class);
+
+		// create one
+		Merchant merchant = buildMerchant("new merchant");
+
+		Merchant savedMerchant = repository.save(merchant);
+
+		Assert.assertTrue(merchant == savedMerchant); // same reference
+		Assert.assertNotNull(merchant.getId());
+		Assert.assertEquals("new merchant", merchant.getName());
+
+		// find by id
+		Merchant foundMerchant = repository.findOne(merchant.getId());
+		Assert.assertNotNull(foundMerchant);
+		Assert.assertEquals(merchant.getId(), foundMerchant.getId());
+		Assert.assertEquals("new merchant", foundMerchant.getName());
+
+		// update
+		foundMerchant.setName("updated name");
+		repository.save(foundMerchant);
+		Merchant updatedMerchant = repository.findOne(merchant.getId());
+		Assert.assertNotNull(updatedMerchant);
+		Assert.assertEquals(merchant.getId(), updatedMerchant.getId());
+		Assert.assertEquals("updated name", updatedMerchant.getName());
+
+		// delete
+		repository.deleteById(merchant.getId());
+		Merchant deletedMerchant = repository.findOne(merchant.getId());
+		Assert.assertNull(deletedMerchant);
+	}
+
+	@Test
+	public void testBatch() {
+		Repository<Merchant> repository = entityManager
+				.getRepository(Merchant.class);
+
+		// add 100
+		List<Merchant> merchants = new ArrayList<Merchant>();
+		for (int i = 0; i < 100; i++) {
+			Merchant merchant = buildMerchant("sample merchant #" + i);
+			merchants.add(merchant);
+		}
+
+		repository.save(merchants);
+
+		// fetch recently added
+		List<Merchant> savedMerchants = repository.findAll();
+
+		Assert.assertNotNull(savedMerchants);
+		Assert.assertEquals(merchants.size(), savedMerchants.size());
+
+		for (Merchant merchant : merchants) {
+			boolean found = false;
+			for (Merchant savedMerchant : savedMerchants) {
+				if (merchant.getId().equals(savedMerchant.getId())) {
+					Assert.assertEquals(merchant.getName(),
+							savedMerchant.getName());
+					found = true;
+					break;
+				}
+			}
+
+			Assert.assertTrue("Merchant not found", found);
+		}
+
+		// update all
+		for (Merchant merchant : merchants) {
+			merchant.setName("updated name");
+		}
+
+		repository.save(merchants);
+
+		// check batch update
+		List<Merchant> updatedMerchants = repository.findAll();
+
+		Assert.assertNotNull(updatedMerchants);
+		Assert.assertEquals(merchants.size(), updatedMerchants.size());
+
+		for (Merchant merchant : merchants) {
+			boolean found = false;
+			for (Merchant updatedMerchant : updatedMerchants) {
+				if (merchant.getId().equals(updatedMerchant.getId())) {
+					Assert.assertEquals("updated name",
+							updatedMerchant.getName());
+					found = true;
+					break;
+				}
+			}
+
+			Assert.assertTrue("Merchant not found", found);
+		}
+
+	}
 
 }
--- src/test/java/org/devspark/aws/lorm/dynamodb/it/schema/SchemaSupportITTest.java
+++ src/test/java/org/devspark/aws/lorm/dynamodb/it/schema/SchemaSupportITTest.java
@@ -3,6 +3,8 @@ package org.devspark.aws.lorm.dynamodb.it.schema;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.devspark.aws.lorm.dynamodb.DynamoDBBaseRepository;
 import org.devspark.aws.lorm.mapping.EntityToItemMapperImpl;
 import org.devspark.aws.lorm.mapping.ItemToEntityMapper;
@@ -10,6 +12,7 @@ import org.devspark.aws.lorm.mapping.ItemToEntityMapperImpl;
 import org.devspark.aws.lorm.schema.validation.SchemaValidationError;
 import org.devspark.aws.lorm.test.model.Expense;
 import org.devspark.aws.lorm.test.model.Merchant;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,81 +23,124 @@ import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.KeyType;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ResourceInUseException;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
 import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.TableDescription;
 
 public class SchemaSupportITTest extends BaseIntegrationTest {
+	private final static Log LOG = LogFactory.getLog(SchemaSupportITTest.class);
 
-    @Before
-    public void setup() {
-	super.setup();
+	@Before
+	public void setup() {
+		super.setup();
 
-	addToEntityManager(Merchant.class);
-	addToEntityManager(Expense.class);
-    }
+		addToEntityManager(Merchant.class);
+		addToEntityManager(Expense.class);
+	}
 
-    @Test
-    public void testValidateSchema() {
-	EntityToItemMapperImpl<Expense> mapper = new EntityToItemMapperImpl<Expense>(Expense.class);
+	@After
+	public void tearDown() {
+		super.tearDown();
+	}
 
-	List<AttributeDefinition> attrDefs = new ArrayList<AttributeDefinition>();
-	attrDefs.add(new AttributeDefinition("id", ScalarAttributeType.S));
+	@Test
+	public void testValidateSchema() {
+		EntityToItemMapperImpl<Expense> mapper = new EntityToItemMapperImpl<Expense>(
+				Expense.class);
 
-	List<KeySchemaElement> keys = new ArrayList<KeySchemaElement>();
-	keys.add(new KeySchemaElement("id", KeyType.HASH));
+		List<AttributeDefinition> attrDefs = new ArrayList<AttributeDefinition>();
+		attrDefs.add(new AttributeDefinition("id", ScalarAttributeType.S));
 
-	ProvisionedThroughput provisionedThroughput = new ProvisionedThroughput();
-	provisionedThroughput.setReadCapacityUnits(1L);
-	provisionedThroughput.setWriteCapacityUnits(1L);
+		List<KeySchemaElement> keys = new ArrayList<KeySchemaElement>();
+		keys.add(new KeySchemaElement("id", KeyType.HASH));
 
-	// create util for table creation
-	CreateTableRequest createTableReq = new CreateTableRequest().withTableName("expense")
-		.withKeySchema(keys);
-	createTableReq.setAttributeDefinitions(attrDefs);
-	createTableReq.setProvisionedThroughput(provisionedThroughput);
+		ProvisionedThroughput provisionedThroughput = new ProvisionedThroughput();
+		provisionedThroughput.setReadCapacityUnits(1L);
+		provisionedThroughput.setWriteCapacityUnits(1L);
 
-	try {
-	    dynamoDB.deleteTable("expense");
-	} catch (Exception ex) {
+		// create util for table creation
+		CreateTableRequest createTableReq = new CreateTableRequest()
+				.withTableName("expense").withKeySchema(keys);
+		createTableReq.setAttributeDefinitions(attrDefs);
+		createTableReq.setProvisionedThroughput(provisionedThroughput);
+
+		deleteTable("expense");
+
+		dynamoDB.createTable(createTableReq);
+
+		// positive case
+		ItemToEntityMapper<Expense> itemToEntityMapper = new ItemToEntityMapperImpl<Expense>(
+				Expense.class, entityManager);
+
+		DynamoDBBaseRepository<Expense> expenseRepository = new DynamoDBBaseRepository<Expense>(
+				new DynamoDB(dynamoDB), mapper, itemToEntityMapper, mapper,
+				Expense.class);
+
+		List<SchemaValidationError> errors = new ArrayList<SchemaValidationError>();
+		Assert.assertTrue(expenseRepository.isValid(errors));
 
 	}
 
-	dynamoDB.createTable(createTableReq);
-
-	// positive case
-	ItemToEntityMapper<Expense> itemToEntityMapper = new ItemToEntityMapperImpl<Expense>(
-		Expense.class, entityManager);
-
-	DynamoDBBaseRepository<Expense> expenseRepository = new DynamoDBBaseRepository<Expense>(
-		new DynamoDB(dynamoDB), mapper, itemToEntityMapper, mapper, Expense.class);
-
-	List<SchemaValidationError> errors = new ArrayList<SchemaValidationError>();
-	Assert.assertTrue(expenseRepository.isValid(errors));
-
-    }
-
-    @Test
-    public void validateSchemaNegativeCase() {
-	try {
-	    dynamoDB.deleteTable("merchant");
-	} catch (Exception ex) {
-
+	private void deleteTable(String tableName) {
+		deleteTable(tableName, 10000);
 	}
 
-	// negative case
-	EntityToItemMapperImpl<Merchant> merchantMapper = new EntityToItemMapperImpl<Merchant>(
-		Merchant.class);
-	ItemToEntityMapper<Merchant> merchantItemToEntityMapper = new ItemToEntityMapperImpl<Merchant>(
-		Merchant.class, entityManager);
+	private void deleteTable(String tableName, int maxWait) {
+		if (maxWait <= 0) {
+			throw new IllegalStateException("Table " + tableName
+					+ " could not be deleted");
+		}
 
-	DynamoDBBaseRepository<Merchant> merchantRepository = new DynamoDBBaseRepository<Merchant>(
-		new DynamoDB(dynamoDB), merchantMapper, merchantItemToEntityMapper, merchantMapper,
-		Merchant.class);
-	List<SchemaValidationError> merchantSchemaValidationErrors = new ArrayList<SchemaValidationError>();
-	Assert.assertFalse(merchantRepository.isValid(merchantSchemaValidationErrors));
-	Assert.assertTrue(merchantRepository.syncToSchema(false, true, false));
-	merchantSchemaValidationErrors.clear();
-	Assert.assertTrue(merchantRepository.isValid(merchantSchemaValidationErrors));
+		try {
+			dynamoDB.deleteTable(tableName);
+		} catch (ResourceNotFoundException ex) {
+		} catch (ResourceInUseException ex) {
+			LOG.warn("Could not delete [" + tableName
+					+ "] table, will try again in 1 second");
+		}
 
-    }
+		TableDescription tableDesc = null;
+		try {
+			tableDesc = dynamoDB.describeTable(tableName).getTable();
+		} catch (ResourceNotFoundException ex) {
+
+		}
+
+		if (tableDesc != null) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+
+			}
+
+			LOG.warn("Going to try again to delete the [" + tableName
+					+ "] table");
+			deleteTable(tableName, maxWait - 1000);
+		}
+	}
+
+	@Test
+	public void validateSchemaNegativeCase() {
+		deleteTable("merchant");
+
+		// negative case
+		EntityToItemMapperImpl<Merchant> merchantMapper = new EntityToItemMapperImpl<Merchant>(
+				Merchant.class);
+		ItemToEntityMapper<Merchant> merchantItemToEntityMapper = new ItemToEntityMapperImpl<Merchant>(
+				Merchant.class, entityManager);
+
+		DynamoDBBaseRepository<Merchant> merchantRepository = new DynamoDBBaseRepository<Merchant>(
+				new DynamoDB(dynamoDB), merchantMapper,
+				merchantItemToEntityMapper, merchantMapper, Merchant.class);
+		List<SchemaValidationError> merchantSchemaValidationErrors = new ArrayList<SchemaValidationError>();
+		Assert.assertFalse(merchantRepository
+				.isValid(merchantSchemaValidationErrors));
+		Assert.assertTrue(merchantRepository.syncToSchema(false, true, false));
+		merchantSchemaValidationErrors.clear();
+		Assert.assertTrue(merchantRepository
+				.isValid(merchantSchemaValidationErrors));
+
+	}
 
 }
